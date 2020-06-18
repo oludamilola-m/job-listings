16de2c14bdf2bb6e220a7b89d7f520cefbaab9af
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-06-18 15:00:45 +0200

    display job list

--- src/App.js
+++ src/App.js
@@ -4,25 +4,11 @@ import { jobs } from "./data";
 import "./App.css";
 
 const App = () => {
-  const job = {
-    id: 1,
-    company: "Photosnap",
-    logo: "https://media.publit.io/file/photosnap.svg",
-    new: true,
-    featured: true,
-    position: "Senior Frontend Developer",
-    role: "Frontend",
-    level: "Senior",
-    postedAt: "1d ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["HTML", "CSS", "JavaScript"],
-    tools: [],
-  };
-
   return (
     <div className="job-listings">
-      <Job {...job} />
+      {jobs.map((job) => {
+        return <Job {...job} key={job.id.toString()} />;
+      })}
     </div>
   );
 };
